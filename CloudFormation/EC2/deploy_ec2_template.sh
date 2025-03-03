b550b517da9c26d86b079ae36b31d4d14e332964
Dmytro Kostenko <kostenko@pythian.com>
2025-03-03 22:11:30 +0000

    Creating template for instance creation

--- CloudFormation/EC2/deploy_ec2_template.sh
+++ CloudFormation/EC2/deploy_ec2_template.sh
@@ -1,7 +1,6 @@
 TEMP_FILE=$1
-STACK_NAME=aws-3tier-app-template-stack
+STACK_NAME=aws-3tier-app-template-ec2-stack
 
 aws cloudformation deploy \
 --template-file $1 \
---no-execute-changeset \
 --stack-name $STACK_NAME 
